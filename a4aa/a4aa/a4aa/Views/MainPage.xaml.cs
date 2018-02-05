5863be341ae307f3d353b26374b9bffdbc906c95
Brian Mize <brian.r.mize@eagles.ewu.edu>
2018-02-05 15:28:19 -0800

    Created a4aa project

---
+++ a4aa/a4aa/a4aa/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace a4aa.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
