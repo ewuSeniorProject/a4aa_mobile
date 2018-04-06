6bdbc3e758c369f1f27909ec6ee01ecdc63999b6
BlakeNoel <blakeenoel@gmail.com>
2018-04-05 20:24:09 -0700

    Continuation of Class Structure.

---
+++ HelloWorld/HelloWorld/SurveyClasses/Survey.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HelloWorld.SurveyClasses.SurveySections;
+
+namespace HelloWorld.SurveyClasses
+{
+    class Survey
+    {
+        private SectionA sectionA;
+        private SectionB sectionB;
+        private SectionC sectionC;
+        private SectionD sectionD;
+        private SectionE sectionE;
+        private SectionF sectionF;
+        private SectionG sectionG;
+        private SectionH sectionH;
+        private SectionI sectionI;
+        private SectionJ sectionJ;
+
+        public SectionA SectionA { get => sectionA; set => sectionA = value; }
+        public SectionB SectionB { get => sectionB; set => sectionB = value; }
+        public SectionC SectionC { get => sectionC; set => sectionC = value; }
+        public SectionD SectionD { get => sectionD; set => sectionD = value; }
+        public SectionE SectionE { get => sectionE; set => sectionE = value; }
+        public SectionF SectionF { get => sectionF; set => sectionF = value; }
+        public SectionG SectionG { get => sectionG; set => sectionG = value; }
+        public SectionH SectionH { get => sectionH; set => sectionH = value; }
+        public SectionI SectionI { get => sectionI; set => sectionI = value; }
+        public SectionJ SectionJ { get => sectionJ; set => sectionJ = value; }
+    }
+}
---
+++ HelloWorld/HelloWorld/SurveyClasses/SurveySections/SectionA.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.SurveyClasses.SurveySections
+{
+    class SectionA
+    {
+
+    }
+}
